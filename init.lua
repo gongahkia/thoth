f2cb80c88a4b79bd7056983ec6c22b0cc3bb68df
gongahkia <angryapplegravy@gmail.com>
2026-02-15 20:45:48 +0800

    Add init.lua entry point re-exporting all src modules

---
+++ init.lua
@@ -0,0 +1,20 @@
+local thoth = {
+    cache = require("src.cache"),
+    events = require("src.events"),
+    graphs = require("src.graphs"),
+    heaps = require("src.heaps"),
+    links = require("src.links"),
+    math = require("src.math"),
+    math2D = require("src.math2D"),
+    queues = require("src.queues"),
+    serialize = require("src.serialize"),
+    stacks = require("src.stacks"),
+    stringify = require("src.stringify"),
+    tables = require("src.tables"),
+    trees = require("src.trees"),
+    tries = require("src.tries"),
+    validate = require("src.validate"),
+    performance = require("src.performance"),
+}
+
+return thoth
