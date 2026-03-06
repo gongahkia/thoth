35aa177bbf1a0cb8ab93d1bad50baed0359f0664
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-03-07 00:08:38 +0800

    test(stringify): cover special and empty delimiters [test/testStringify.lua]

--- test/testStringify.lua
+++ test/testStringify.lua
@@ -7,6 +7,15 @@ assert(s.Rstrip("   hi   ") == "   hi")
 local parts = s.Split("a,b,c", ",")
 assert(#parts == 3 and parts[1] == "a" and parts[3] == "c")
 
+local dotted = s.Split("a.b.c", ".")
+assert(#dotted == 3 and dotted[2] == "b")
+
+local stars = s.Split("a**b**c", "**")
+assert(#stars == 3 and stars[1] == "a" and stars[3] == "c")
+
+local chars = s.Split("abc", "")
+assert(#chars == 3 and chars[1] == "a" and chars[3] == "c")
+
 assert(s.StartsWith("hello", "he"))
 assert(s.EndsWith("hello", "lo"))
 assert(s.Contains("hello", "ell"))
